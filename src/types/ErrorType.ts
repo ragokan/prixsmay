9da98674a2cb90a0964c56fc7fee0e31c14f8c8b
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-22 22:44:12 +0300

    create errors

---
+++ src/types/ErrorType.ts
@@ -0,0 +1,4 @@
+export interface ErrorType {
+  message: string;
+  statusCode: number;
+}
--- src/utils/ErrorHandler.ts
+++ src/utils/ErrorHandler.ts
@@ -1,17 +1,18 @@
+import { NextFunction, Request, Response } from "express";
+import { ErrorType } from "../types/ErrorType.js";
 import ErrorObject from "./ErrorObject.js";
 
-const ErrorHandler = (err, req, res, next) => {
-  console.log(err);
+const ErrorHandler = (err: ErrorType, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.message);
 
   res.status(err.statusCode || 500);
   res.json({
     message: err.message || "Server Error",
-    translateCode: err.translateCode && err.translateCode,
   });
 };
 
-const NotFound = (req, res, next) => {
-  const NotFoundError = new ErrorObject(`The url you wanted to see '${req.originalUrl}' is not found`, 404, "404");
+const NotFound = (req: Request, res: Response, next: NextFunction) => {
+  const NotFoundError = new ErrorObject(`The url you wanted to see '${req.originalUrl}' is not found`, 404);
   next(NotFoundError);
 };
 
