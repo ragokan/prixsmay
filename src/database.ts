7d623ded9e0a53701ce5d32dab7772d90f57c092
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-24 00:54:03 +0300

    create first test

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { testMode } from "./tests/testMode";
 
-const prisma = testMode
-  ? new PrismaClient({ datasources: { db: { url: process.env.TEST_DATABASE_URL } } })
-  : new PrismaClient();
+const prisma = new PrismaClient();
 const { user } = prisma;
 
 export { prisma, user as User };
---
+++ src/startServer.ts
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { ErrorHandler, NotFound } from "./utils/ErrorHandler";
+import formData from "express-form-data";
+import { prisma } from "./database";
+import routing from "./routing";
+import session from "./session";
+import express from "express";
+import cors from "cors";
+
+export const startServer = async () => {
+  try {
+    await prisma.$connect().then(() => console.log("Successfully connected to the database!"));
+
+    const app = express();
+    app.use(cors({ credentials: true, origin: process.env.frontendUrl }));
+    app.use(express.json());
+    app.use(formData.parse());
+
+    // Routes - Session
+    session(app);
+    routing(app);
+
+    // Middleware
+    app.use(NotFound);
+    app.use(ErrorHandler);
+
+    // Server
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => console.log(`The server is currently running on port ${PORT}!`));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
