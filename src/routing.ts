6b63bfa10de8d77bff97629cc9235f761bfec7a5
ragokan <okanyldrmop@gmail.com>
2021-02-06 00:49:31 +0300

    show server is running when you go to /

--- src/routing.ts
+++ src/routing.ts
@@ -1,8 +1,15 @@
 import AuthRoutes from "./routes/AuthRoutes";
 import PostRoutes from "./routes/PostRoutes";
 import { Application } from "express";
+import { InlineType } from "./utils/InlineType";
+import { IResponse } from "./types/ResponseTypes";
 
 export default (app: Application) => {
   app.use("/api/auth", AuthRoutes);
   app.use("/api/post", PostRoutes);
+  app.get("/", (_, res, __) =>
+    res.status(200).json(
+      InlineType<IResponse>({ message: "Prixsmay is running!", success: true })
+    )
+  );
 };
