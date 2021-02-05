9a3b6f6181d0d413acde12b09ecfbe5d64ba099e
ragokan <okanyldrmop@gmail.com>
2021-02-06 00:13:05 +0300

    get single post

---
+++ src/controllers/Post/GetSinglePost.ts
@@ -0,0 +1,29 @@
+import { RequestContext, ResponseContext } from "../../types/ExpressTypes";
+import Async from "../../middleware/Async";
+import { NextFunction } from "express";
+import { InlineType } from "../../utils/InlineType";
+import { IPostResponse } from "../../types/ResponseTypes";
+import { Post } from "../../database";
+
+interface ReqBody extends RequestContext {
+  params: {
+    id: string;
+  };
+}
+
+export const GetSinglePostFunction = Async(async (req: ReqBody, res: ResponseContext, _: NextFunction) => {
+  const post = await Post.findUnique({
+    where: { id: parseInt(req.params.id) },
+    include: { author: { select: { email: true, name: true, id: true } }, votes: true },
+  });
+
+  res
+    .status(200)
+    .json(
+      InlineType<IPostResponse>(
+        post
+          ? { message: "Post is received successfully!", success: true, post }
+          : { message: "No post is found with this id!", success: false }
+      )
+    );
+});
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { CreatePostFunction } from "../controllers/Post/CreatePost";
 import { GetPostsFunction } from "../controllers/Post/GetPosts";
+import { GetSinglePostFunction } from "../controllers/Post/GetSinglePost";
 import { LoginRequired } from "../middleware/Authentication";
 
 const router = express.Router();
 
 router.route("/").post(LoginRequired, CreatePostFunction).get(GetPostsFunction);
+router.route("/:id").get(GetSinglePostFunction);
 
 export default router;
