0b3f5dc2a2553c39760c873c7f0ec41c2aed7f65
ragokan <okanyldrmop@gmail.com>
2021-02-05 22:53:39 +0300

    add new fields

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
-const { user } = prisma;
+const { user, post, vote } = prisma;
 
 export { prisma, user as User };
