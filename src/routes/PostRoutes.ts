91002c57cc664dab34138e6735f9420dd166fe5e
ragokan <okanyldrmop@gmail.com>
2021-02-07 21:05:17 +0300

    route names

--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { CreatePostFunction } from "../controllers/Post/CreatePost";
-import { DeletePostFunction } from "../controllers/Post/DeletePost";
-import { GetPostsFunction } from "../controllers/Post/GetPosts";
-import { GetSinglePostFunction } from "../controllers/Post/GetSinglePost";
-import { UpdatePostFunction } from "../controllers/Post/UpdatePost";
+import { CreatePostFunction } from "../controllers/Post/CreatePostController";
+import { DeletePostFunction } from "../controllers/Post/DeletePostController";
+import { GetPostsFunction } from "../controllers/Post/GetPostsController";
+import { GetSinglePostFunction } from "../controllers/Post/GetSinglePostController";
+import { UpdatePostFunction } from "../controllers/Post/UpdatePostController";
 import { LoginRequired } from "../middleware/Authentication";
 
 const router = express.Router();
