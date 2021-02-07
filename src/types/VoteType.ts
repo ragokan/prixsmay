14b0885161ba0533db68f4e2f1b14c2128371f86
ragokan <okanyldrmop@gmail.com>
2021-02-07 22:08:27 +0300

    update vote type

--- src/types/VoteType.ts
+++ src/types/VoteType.ts
@@ -1,9 +1,7 @@
-export const VoteType = {
-  up: "up",
-  down: "down",
-};
-
-export type VoteType = typeof VoteType[keyof typeof VoteType];
+export interface VoteType {
+  up: "up";
+  down: "down";
+}
 
 export interface IVote {
   id: number;
