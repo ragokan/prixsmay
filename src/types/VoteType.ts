3c3cf7cb55a396946f16d42652fdb9f700291208
ragokan <okanyldrmop@gmail.com>
2021-02-08 13:01:54 +0300

    revert vote type

--- src/types/VoteType.ts
+++ src/types/VoteType.ts
@@ -1,7 +1,9 @@
-export interface VoteType {
-  up: "up";
-  down: "down";
-}
+export const VoteType = {
+  up: "up",
+  down: "down",
+};
+
+export type VoteType = typeof VoteType[keyof typeof VoteType];
 
 export interface IVote {
   id: number;
