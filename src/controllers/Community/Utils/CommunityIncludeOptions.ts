7c46c7a22107d08bba63607420cac0ea7e14d0c9
ragokan <okanyldrmop@gmail.com>
2021-03-08 00:52:43 +0300

    update community select

--- src/controllers/Community/Utils/CommunityIncludeOptions.ts
+++ src/controllers/Community/Utils/CommunityIncludeOptions.ts
@@ -1,13 +1,15 @@
-import { Prisma } from "@prisma/client"
-import { postIncludeOptions } from "../../Post/Utils/PostIncludeOptions"
-import { userSelectOptions } from "../../User/Utils/UserSelectOptions"
+import { Prisma } from "@prisma/client";
+import { postIncludeOptions } from "../../Post/Utils/PostIncludeOptions";
+import { userSelectOptions } from "../../User/Utils/UserSelectOptions";
 
 export const communityIncludeOptions = (limit: number = 10, page: number = 1): Prisma.CommunityInclude => ({
   members: { select: userSelectOptions },
   posts: {
     include: { ...postIncludeOptions, community: false },
     orderBy: { createdAt: "desc" },
-    skip: page ? (page - 1) * 10 : 0,
     take: limit || 10,
+    skip: page ? (page - 1) * 10 : 0,
   },
-})
+});
+
+export const communitySelectOptions: Prisma.CommunitySelect = { name: true, description: true, picture: true };
--- src/controllers/User/Utils/UserIncludeOptions.ts
+++ src/controllers/User/Utils/UserIncludeOptions.ts
@@ -1,10 +1,12 @@
-import { Prisma } from "@prisma/client"
-import { commentIncludeOptions } from "../../Post/Comment/Utils/CommentIncludeOptions"
-import { postIncludeOptions } from "../../Post/Utils/PostIncludeOptions"
+import { Prisma } from "@prisma/client";
+import { communitySelectOptions } from "../../Community/Utils/CommunityIncludeOptions";
+import { commentIncludeOptions } from "../../Post/Comment/Utils/CommentIncludeOptions";
+import { postIncludeOptions } from "../../Post/Utils/PostIncludeOptions";
 
-export const userProfileArgs: Prisma.ProfileArgs = { select: { picture: true, about: true } }
+export const userProfileArgs: Prisma.ProfileArgs = { select: { picture: true, about: true } };
 export const userIncludeOptions: Prisma.UserInclude = {
   posts: { include: postIncludeOptions },
   profile: userProfileArgs,
   comments: { include: commentIncludeOptions },
-}
+  communities: { select: communitySelectOptions },
+};
