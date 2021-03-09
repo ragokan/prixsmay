781fc0570704d0dd038574d42cd3206af5fb04ff
ragokan <okanyldrmop@gmail.com>
2021-03-09 22:06:56 +0300

    upload profile picture

--- src/controllers/User/AddProfileController.ts
+++ src/controllers/User/AddProfileController.ts
@@ -1,41 +1,55 @@
-import { NextFunction } from "express"
-import { omit } from "lodash"
-import { defaultUserPictureConstants } from "../../constants/CloudinaryConstants"
-import { User } from "../../database"
-import Async from "../../middleware/Async"
-import { RequestContext, ResponseContext } from "../../types/ExpressTypes"
-import { FileType } from "../../types/FileType"
-import { IUserResponse } from "../../types/ResponseTypes"
-import ErrorObject from "../../utils/ErrorObject"
-import { FindImageName } from "../../utils/FindImageNameRegex"
-import { InlineType } from "../../utils/InlineType"
-import { UploadImage } from "../../utils/UploadImage"
-import { useCloudinary } from "../../utils/UseCloudinary"
-import { userIncludeOptions } from "./Utils/UserIncludeOptions"
+import { NextFunction } from "express";
+import { omit } from "lodash";
+import { defaultUserPictureConstants } from "../../constants/CloudinaryConstants";
+import { User } from "../../database";
+import Async from "../../middleware/Async";
+import { RequestContext, ResponseContext } from "../../types/ExpressTypes";
+import { FileType } from "../../types/FileType";
+import { IUserResponse } from "../../types/ResponseTypes";
+import ErrorObject from "../../utils/ErrorObject";
+import { FindImageName } from "../../utils/FindImageNameRegex";
+import { InlineType } from "../../utils/InlineType";
+import { UploadImage } from "../../utils/UploadImage";
+import { useCloudinary } from "../../utils/UseCloudinary";
+import { userIncludeOptions } from "./Utils/UserIncludeOptions";
 
 interface ReqBody extends RequestContext {
-  files: FileType
+  files: FileType;
+  body: {
+    pictureUrl?: string;
+  };
 }
 
 export const AddProfileFunction = Async(async (req: ReqBody, res: ResponseContext, next: NextFunction) => {
-  const { image } = req.files
-  if (!image || !image.type.includes("image"))
-    return next(new ErrorObject("Please provide an image to update your profile!", 400))
+  const image = req.files?.image || undefined;
 
-  // Delete Old Profile Picture
-  const userCheck = await User.findUnique({ where: { id: req.session.userId }, include: { profile: true } })
-  const userCurrentImage = FindImageName(userCheck!.profile!.picture)
-  if (userCurrentImage && !defaultUserPictureConstants.includes(userCurrentImage))
-    await useCloudinary.uploader.destroy(userCurrentImage)
+  let user;
 
-  const picture = await UploadImage(image.path)
+  if (req.body.pictureUrl) {
+    user = await User.update({
+      where: { id: req.session.userId },
+      include: userIncludeOptions,
+      data: { profile: { update: { picture: req.body.pictureUrl } } },
+    });
+  } else if (image) {
+    if (!image.type.includes("image"))
+      return next(new ErrorObject("Please provide an image to update your profile!", 400));
+    // Delete Old Profile Picture
+    const userCheck = await User.findUnique({ where: { id: req.session.userId }, include: { profile: true } });
+    const userCurrentImage = FindImageName(userCheck!.profile!.picture);
+    if (userCurrentImage && !defaultUserPictureConstants.includes(userCurrentImage))
+      await useCloudinary.uploader.destroy(userCurrentImage);
 
-  const user = await User.update({
-    where: { id: req.session.userId },
-    include: userIncludeOptions,
-    data: { profile: { update: { picture } } },
-  })
-  const filteredUser = omit(user, ["password", "isActivated"])
+    const picture = await UploadImage(image.path);
+
+    user = await User.update({
+      where: { id: req.session.userId },
+      include: userIncludeOptions,
+      data: { profile: { update: { picture } } },
+    });
+  } else return next(new ErrorObject("Please provide an image to update your profile!", 400));
+
+  const filteredUser = omit(user, ["password", "isActivated"]);
 
   res.status(200).json(
     InlineType<IUserResponse>({
@@ -43,5 +57,5 @@ export const AddProfileFunction = Async(async (req: ReqBody, res: ResponseContex
       success: true,
       user: filteredUser,
     })
-  )
-})
+  );
+});
--- src/testing/tests/auth/GetUser.test.ts
+++ src/testing/tests/auth/GetUser.test.ts
@@ -1,55 +1,55 @@
-import { prisma } from "../../../database"
-import fetch from "node-fetch"
-import { testUrl } from "../../utils/TestConstants"
-import { StartTest } from "../../utils/StartTest"
-import { TestAccount } from "../../utils/TestAccount"
-import { Server } from "http"
-// import bcrypt from "bcryptjs"
+import { prisma } from "../../../database";
+import fetch from "node-fetch";
+import { testUrl } from "../../utils/TestConstants";
+import { StartTest } from "../../utils/StartTest";
+import { TestAccount } from "../../utils/TestAccount";
+import { Server } from "http";
+// import bcrypt from "bcryptjs";
 
-let server: Server
+let server: Server;
 beforeAll(async () => {
-  server = await StartTest()
-})
+  server = await StartTest();
+});
 
 afterAll(async () => {
   if (server)
     server.close((err: any) => {
-      if (err) return console.log(err)
-    })
-})
+      if (err) return console.log(err);
+    });
+});
 
 describe("Get User", () => {
-  const user = TestAccount
-  let dbUser: any
-  let cookie: any
+  const user = TestAccount;
+  let dbUser: any;
+  let cookie: any;
 
   it("find user", async () => {
-    // const pass = await bcrypt.hash(TestAccount.password, 12)
-    // await prisma.user.create({ data: { ...TestAccount, password: pass, isActivated: true } })
-    dbUser = await prisma.user.findUnique({ where: { email: user.email } })
-  })
+    // const pass = await bcrypt.hash(TestAccount.password, 12);
+    // await prisma.user.create({ data: { ...TestAccount, password: pass, isActivated: true, type: "admin" } });
+    dbUser = await prisma.user.findUnique({ where: { email: user.email } });
+  });
 
   it("login user", async () => {
     const body = {
       email: user.email,
       password: user.password,
-    }
+    };
 
     const responseData = await fetch(testUrl + "auth/login", {
       method: "POST",
       body: JSON.stringify(body),
       headers: { "Content-Type": "application/json" },
-    })
-    cookie = responseData.headers.get("set-cookie")
-  })
+    });
+    cookie = responseData.headers.get("set-cookie");
+  });
 
   it("get user", async () => {
     const responseData = await fetch(testUrl + "user", {
       method: "GET",
       headers: { cookie },
-    })
-    expect(responseData.status).toBe(200)
-    const response = await responseData.json()
+    });
+    expect(responseData.status).toBe(200);
+    const response = await responseData.json();
 
     expect(response).toMatchObject({
       message: "User info is received successfully!",
@@ -59,6 +59,6 @@ describe("Get User", () => {
         username: user.username,
         email: user.email,
       },
-    })
-  })
-})
+    });
+  });
+});
